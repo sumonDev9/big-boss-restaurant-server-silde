831958b80aaabc5e32af8a07b1c6b0bd655d737b
sumonDev9 <sumonmit678@gmail.com>
2025-01-02 15:22:35 +0530

    Implemented `DELETE /carts/:id` route to remove items from the cart

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.POST || 5000;  
 
 // middleware
@@ -61,6 +61,12 @@ async function run() {
     })
 
 
+    app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
