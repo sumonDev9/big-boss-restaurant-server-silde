35e5112699c6dc4bc5b0dc5bcaca5a656ce8dc05
sumonDev9 <sumonmit678@gmail.com>
2025-01-06 13:46:44 +0530

    Add API to delete a user by ID

--- index.js
+++ index.js
@@ -86,12 +86,14 @@ async function run() {
       res.send(result)
     })
 
+    
     app.delete('/users/:id', async(req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await userCollection.deleteOne(query);
       res.send(result);
     })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
