3e5a371c1f25d636df354c314069833a2fdd7b86
sumonDev9 <sumonmit678@gmail.com>
2025-01-07 00:10:37 +0530

    Add verifyAdmin middleware to restrict access to admin-only routes

--- index.js
+++ index.js
@@ -49,12 +49,12 @@ async function run() {
     const verifyToken = (req, res, next) => {
       console.log('inside verify token', req.headers.authorization); 
       if(!req.headers.authorization){
-        return res.status(401).send({message: 'forbidden access'})
+        return res.status(401).send({message: 'unauthorized access'})
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if(err){
-          return res.status(401).send({message: 'forbidden access'})
+          return res.status(401).send({message: 'unauthorized access'})
         }
         req.decoded = decoded;
         next();
@@ -96,8 +96,22 @@ async function run() {
       res.send(result);
     })
 
+
+
     // user related api
-    app.get('/users', verifyToken, async(req, res) => {
+      // use verify admin after verifyToken 
+      const verifyAdmin = async(req, res, next) => {
+        const email = req.decoded.email;
+        const query = {email: email}
+        const user = await userCollection.findOne(query)
+        const isAdmin = user?.role === 'admin';
+        if(!isAdmin){
+          return res.status(403).send({message: 'forbidden access'})
+        }
+        next();
+      }
+
+    app.get('/users', verifyToken, verifyAdmin, async(req, res) => {
       // console.log(req.headers); // jwt token check
       const result = await userCollection.find().toArray();
       res.send(result);
@@ -114,10 +128,12 @@ async function run() {
       res.send(result)
     })
 
+  
+
     app.get('/users/admin/:email', verifyToken, async(req, res) => {
       const email = req.params.email;
       if(email !== req.decoded.email){
-        return res.status(403).send({message: 'unauthorized access'})
+        return res.status(403).send({message: 'orbidden access'})
       }
       const query = {email: email}
       const user = await userCollection.findOne(query);
@@ -128,7 +144,7 @@ async function run() {
       res.send({admin})
     })
 
-    app.patch('/users/admin/:id', async (req, res) => {
+    app.patch('/users/admin/:id',verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)}
       const updatedDoc = {
@@ -140,7 +156,7 @@ async function run() {
       res.send(result);
     })
     
-    app.delete('/users/:id', async(req, res) => {
+    app.delete('/users/:id',verifyToken,verifyAdmin, async(req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}
       const result = await userCollection.deleteOne(query);
