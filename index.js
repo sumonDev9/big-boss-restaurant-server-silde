93e74f9cc97fd62e521ed34956db22ae809e1b63
sumonDev9 <sumonmit678@gmail.com>
2025-01-08 23:04:16 +0530

    Add '/menu/:id' DELETE route with verifyToken and verifyAdmin for secure menu item deletion

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/menu/:id', async(req, res) => {
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id)}
       const result = await menuCollection.deleteOne(query);
