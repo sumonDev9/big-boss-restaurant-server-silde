deb49aa9e6489e7d022fbe29d4faab0139c22e79
sumonDev9 <sumonmit678@gmail.com>
2025-01-08 23:38:40 +0530

    Add '/menu/:id' GET route to fetch menu item details by ID

--- index.js
+++ index.js
@@ -86,6 +86,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/menu/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    })
+
     app.delete('/menu/:id', verifyToken, verifyAdmin, async(req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id)}
